82ad3d143efe01531a25df1b14ee0321c1602a71
hostnetwg <hostnet.wg@gmail.com>
2025-03-08 12:42:33 +0100

    Naprawa formularzy dodawania i educji uczestników szkolenia

--- resources/views/participants/create.blade.php
+++ resources/views/participants/create.blade.php
@@ -9,14 +9,15 @@
         <form action="{{ route('participants.store', $course) }}" method="POST">
             @csrf
 
-            <div class="mb-3">
-                <label for="first_name" class="form-label">Imię</label>
-                <input type="text" name="first_name" class="form-control" id="first_name" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="last_name" class="form-label">Nazwisko</label>
-                <input type="text" name="last_name" class="form-control" id="last_name" required>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <label for="first_name" class="form-label">Imię</label>
+                    <input type="text" name="first_name" class="form-control" id="first_name" required>
+                </div>
+                <div class="col-md-6">
+                    <label for="last_name" class="form-label">Nazwisko</label>
+                    <input type="text" name="last_name" class="form-control" id="last_name" required>
+                </div>
             </div>
 
             <div class="mb-3">
@@ -24,17 +25,19 @@
                 <input type="email" name="email" class="form-control" id="email">
             </div>
 
-            <div class="mb-3">
-                <label for="birth_date" class="form-label">Data urodzenia</label>
-                <input type="date" name="birth_date" class="form-control" id="birth_date">
-            </div>
-
-            <div class="mb-3">
-                <label for="birth_place" class="form-label">Miejsce urodzenia</label>
-                <input type="text" name="birth_place" class="form-control" id="birth_place">
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <label for="birth_date" class="form-label">Data urodzenia</label>
+                    <input type="date" name="birth_date" class="form-control" id="birth_date">
+                </div>
+                <div class="col-md-6">
+                    <label for="birth_place" class="form-label">Miejsce urodzenia</label>
+                    <input type="text" name="birth_place" class="form-control" id="birth_place">
+                </div>
             </div>
 
             <button type="submit" class="btn btn-success">Dodaj uczestnika</button>
+            <a href="javascript:history.back()" class="btn btn-secondary">Anuluj</a>
         </form>
     </div>
 </x-app-layout>
--- resources/views/participants/edit.blade.php
+++ resources/views/participants/edit.blade.php
@@ -1,47 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="fw-semibold fs-4 text-dark">
-            {{ __('Edytuj szkolenie') }}
+            {{ __('Edycja danych uczestnika') }}
         </h2>
     </x-slot>
 
     <div class="px-3 py-3">
         <div class="container">
             <!-- Formularz edycji kursu -->
-            <form action="{{ route('courses.update', $course->id) }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('participants.update', ['course' => $participant->course_id, 'participant' => $participant->id]) }}" method="POST">
                 @csrf
                 @method('PUT')
-
-                <div class="mb-3">
-                    <label for="title" class="form-label">Tytuł kursu</label>
-                    <input type="text" name="title" id="title" class="form-control" value="{{ $course->title }}" required>
-                </div>
-
-                <div class="mb-3">
-                    <label for="description" class="form-label">Opis</label>
-                    <textarea name="description" id="description" class="form-control">{{ $course->description }}</textarea>
-                </div>
-
+    
+                <input type="hidden" name="course_id" value="{{ $participant->course_id }}">
+    
                 <div class="row mb-3">
-                    <div class="col-md-3">
-                        <label for="start_date" class="form-label">Data rozpoczęcia</label>
-                        <input type="date" name="start_date" id="start_date" class="form-control" value="{{ $course->start_date }}">
+                    <div class="col-md-6">
+                        <label for="first_name" class="form-label">Imię</label>
+                        <input type="text" name="first_name" id="first_name" class="form-control" value="{{ old('first_name', $participant->first_name) }}" required>
                     </div>
-                    <div class="col-md-3">
-                        <label for="end_date" class="form-label">Data zakończenia</label>
-                        <input type="date" name="end_date" id="end_date" class="form-control" value="{{ $course->end_date }}">
+                    <div class="col-md-6">
+                        <label for="last_name" class="form-label">Nazwisko</label>
+                        <input type="text" name="last_name" id="last_name" class="form-control" value="{{ old('last_name', $participant->last_name) }}" required>
                     </div>
                 </div>
-
+    
                 <div class="mb-3">
-                    <label for="image" class="form-label">Zdjęcie kursu</label>
-                    <input type="file" name="image" id="image" class="form-control">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $participant->email) }}">
                 </div>
-
-                <div class="text-end">
-                    <button type="submit" class="btn btn-primary">Zapisz zmiany</button>
-                    <a href="{{ route('courses.index') }}" class="btn btn-secondary">Anuluj</a>
+    
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <label for="birth_date" class="form-label">Data urodzenia</label>
+                        <input type="date" name="birth_date" id="birth_date" class="form-control" value="{{ old('birth_date', $participant->birth_date) }}">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="birth_place" class="form-label">Miejsce urodzenia</label>
+                        <input type="text" name="birth_place" id="birth_place" class="form-control" value="{{ old('birth_place', $participant->birth_place) }}">
+                    </div>
                 </div>
+    
+                <button type="submit" class="btn btn-primary">Zapisz zmiany</button>
+                <a href="javascript:history.back()" class="btn btn-secondary">Anuluj</a>                
             </form>
         </div>
     </div>
