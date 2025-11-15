fc69ab7843bb33fa122893fe2f5c71ee6bc9cec2
hostnetwg <hostnet.wg@gmail.com>
2025-11-15 01:28:53 +0100

    Poprawki na stronie listy zamówień v4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use App\Models\FormOrder;
 use App\Observers\FormOrderObserver;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Database\Events\ConnectionEstablished;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,15 @@ class AppServiceProvider extends ServiceProvider
         
         // Rejestracja Observer dla automatycznego zapisu uczestników
         FormOrder::observe(FormOrderObserver::class);
+        
+        // Wymuś ustawienie strefy czasowej MySQL na UTC przy każdym połączeniu
+        // To rozwiązuje problem z różnymi strefami czasowymi między środowiskami
+        Event::listen(ConnectionEstablished::class, function (ConnectionEstablished $event) {
+            if ($event->connection->getDriverName() === 'mysql') {
+                // Ustaw strefę czasową sesji MySQL na UTC
+                // To zapewni, że MySQL zwraca daty w UTC, niezależnie od konfiguracji serwera
+                $event->connection->statement("SET time_zone = '+00:00'");
+            }
+        });
     }
 }
