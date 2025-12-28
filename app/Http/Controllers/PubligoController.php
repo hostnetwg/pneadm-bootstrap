7474f8750d55ec43499b17b3e2e2713fcad4e084
hostnetwg <hostnet.wg@gmail.com>
2025-12-29 00:56:49 +0100

    Dodanie funkcji do webhooka PUBLIGO v6

--- app/Http/Controllers/PubligoController.php
+++ app/Http/Controllers/PubligoController.php
@@ -416,17 +416,15 @@ class PubligoController extends Controller
                     $sendyService = new SendyService();
                     $sendyListId = 'dncdl0kfUMnk43BysMa892NQ'; // ID listy "UCZESTNICY PŁATNYCH SZKOLEŃ"
                     
-                    // Przygotuj pełne imię i nazwisko dla standardowego pola SENDY
-                    $fullName = trim($customer['first_name'] . ' ' . $customer['last_name']);
-                    
                     // Pobierz datę szkolenia (start_date kursu) - format RRRR-MM-DD
                     $courseDate = $course->start_date ? $course->start_date->format('Y-m-d') : now()->format('Y-m-d');
                     
                     // Przygotuj pola dla SENDY
                     // Standardowe pola SENDY: 'name' (małe litery w API, wyświetlane jako "Name" w interfejsie)
                     // Custom fields: muszą mieć dokładnie takie same nazwy jak w SENDY (case-sensitive)
+                    // Dla listy "UCZESTNICY PŁATNYCH SZKOLEŃ": Name = tylko imię, Sername = nazwisko
                     $sendyFields = [
-                        'name' => $fullName, // Standardowe pole SENDY - pełne imię i nazwisko (małe litery w API)
+                        'name' => $customer['first_name'], // Standardowe pole SENDY - tylko imię (małe litery w API)
                         'Sername' => $customer['last_name'], // Custom field - nazwisko (dokładnie jak w SENDY)
                         'data' => $courseDate, // Custom field - data szkolenia (małe litery, jak w SENDY)
                         'id_szkolenia' => (string) $course->id, // Custom field - ID szkolenia (dokładnie jak w SENDY)
