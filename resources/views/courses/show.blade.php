18a979c0f881de5b6124b582d13ffb97094486cc
hostnetwg <hostnet.wg@gmail.com>
2026-03-28 13:15:17 +0100

    Dodanie opcji wyświetlania filmów TIK z YouTube v1

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -761,6 +761,7 @@
                                             frameborder="0" 
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                                             allowfullscreen
+                                            referrerpolicy="strict-origin-when-cross-origin"
                                             loading="lazy">
                                         </iframe>
                                     </div>
